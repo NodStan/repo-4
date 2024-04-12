7f403d3745a85b2a7ec718b7c97dd76ed40e1c27
Stanley <stanleyobitunwase@gmail.com>
2024-04-12 13:49:47 +0100

    assignment2

---
+++ Chelsea.java
@@ -0,0 +1,13 @@
+//package assignment2;
+//
+//import java.util.ArrayList;
+//
+//public class Chelsea implements footBallClub{
+//    public ArrayList<Player>playerList(){
+//        ArrayList<Player> arsenalPlayers = new ArrayList<>();
+//        arsenalPlayers.add(new Player("dfj", "ijfds"));
+//        arsenalPlayers.add(new Player("dfj", "ijfds"));
+//        arsenalPlayers.add(new Player("dfj", "ijfds"));
+//        return arsenalPlayers;
+//    }
+//}
